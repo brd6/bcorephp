b3adf00a1dac910ce0849ad3a6a4aa9d029364c8
bberd <b.berdrigue@gmail.com>
2016-07-29 01:14:48 +0200

    dao

---
+++ Core/DAO.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bberd
+ * Date: 29/07/2016
+ * Time: 01:07
+ */
+
+namespace Bcorephp;
+
+
+abstract class DAO
+{
+    /**
+     * Db instance
+     * @var
+     */
+    private $db;
+
+    public function __construct($db)
+    {
+        $this->db = $db;
+    }
+
+    // crud
+    protected abstract function create();
+    protected abstract function update();
+    protected abstract function delete();
+}
